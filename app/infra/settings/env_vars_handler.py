dd810a43419bd681c9c29378cbc5da2169b45c1c
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-08 06:26:33 -0300

    create: infrastructure setting: environment vars handler

---
+++ app/infra/settings/env_vars_handler.py
@@ -0,0 +1,25 @@
+from os import getenv
+from dotenv import load_dotenv
+
+
+class EnvVarsHandler:
+
+    @classmethod
+    def read_env_vars(cls, dot_env_file: str):
+        load_dotenv(dotenv_path=dot_env_file)
+
+    @staticmethod
+    def verify_env(func):
+        def wrapper(*args, **kwargs):
+            try:
+                action = func(*args, **kwargs)
+                return action
+            except EnvironmentError as e:
+                return f"Error: {str(e)}"
+
+        return wrapper
+
+    @staticmethod
+    @verify_env
+    def get_db_connection_string():
+        return getenv('DB_CONNECTION_STRING')
