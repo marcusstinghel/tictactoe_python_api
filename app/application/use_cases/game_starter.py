6589f631a3569a063e719b2fda7d2153b986c55c
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-09 16:33:44 -0300

    create: game starter use case to application

---
+++ app/application/use_cases/game_starter.py
@@ -0,0 +1,18 @@
+from app.domain.entities import Game
+from app.domain.repositories import GameRepositoryInterface
+
+
+class GameStarter:
+    def __init__(self, game_repository: GameRepositoryInterface):
+        self.__game_repository = game_repository
+
+    def start(self, player_id: int) -> Game:
+        registered_game = self.__game_repository.create_game(
+            player_id=player_id,
+            board=([0, 0, 0], [0, 0, 0], [0, 0, 0]),
+            state='in_progress',
+            is_player_winner=False,
+            movements=None,
+        )
+        return registered_game
+
