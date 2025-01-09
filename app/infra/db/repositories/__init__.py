e1ccd2ccd161bff87fceac828cccfae4a1e07b66
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-09 16:45:15 -0300

    modify: package importations to database repositories

--- app/infra/db/repositories/__init__.py
+++ app/infra/db/repositories/__init__.py
@@ -1,1 +1,2 @@
-from .player_repository import PlayerRepository
+from app.infra.db.repositories.player_repository import PlayerRepository
+from app.infra.db.repositories.game_repository import GameRepository
