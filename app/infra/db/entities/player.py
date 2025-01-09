3fb745e931c4bf06c4ef1bf4dbeaec6e1338f1bf
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-09 16:42:26 -0300

    feat: added stats and relationship to player database entitie

--- app/infra/db/entities/player.py
+++ app/infra/db/entities/player.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.infra.db.entities import Base
 
@@ -10,6 +11,11 @@ class Player(Base):
     first_name = Column(String, index=True)
     last_name = Column(String)
     nickname = Column(String, unique=True)
+    victories = Column(Integer, nullable=True)
+    defeats = Column(Integer, nullable=True)
+    draws = Column(Integer, nullable=True)
+
+    games = relationship('Game', back_populates='player')
 
     def __repr__(self):
-        return f"<Player(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, nickname={self.nickname})>"
+        return f"<Player(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, nickname={self.nickname}, victories={self.victories}, defeats={self.defeats}, draws={self.draws})>"
