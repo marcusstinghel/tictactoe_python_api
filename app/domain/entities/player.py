b85c7d1993c4dbe939cb6d3c17637bcda8f553a9
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-09 16:24:06 -0300

    create: player domain entity

---
+++ app/domain/entities/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, id: int, first_name: str, last_name: str, nickname: str, victories: int, defeats: int, draws: int):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.victories = victories
+        self.defeats = defeats
+        self.draws = draws
