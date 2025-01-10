4ff38bd0b0d748d7f34bde94fedc50145b60f1a7
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-10 00:14:36 -0300

    feat: draw check

--- app/domain/services/game_service.py
+++ app/domain/services/game_service.py
@@ -21,8 +21,10 @@ class GameService:
     def make_move(self) -> Game:
         self.__update_board()
         self.__update_movements()
-        if self.__check_win():
-            self.__declare_winner()
+        is_draw = self.__check_draw()
+        is_winner = self.__check_win()
+        if is_winner or is_draw:
+            self.__declare_winner(is_draw)
         return self.__game
 
     def __update_board(self):
@@ -35,12 +37,23 @@ class GameService:
                 return True
             if all(self.__game.board[0][i] == self.__game.board[j][i] != 0 for j in range(3)):
                 return True
-            if all(self.__game.board[i][i] == 1 for i in range(3)) or all(self.__game.board[i][2 - i] == 0 for i in range(3)):
+            if self.__game.board[0][0] == self.__game.board[1][1] == self.__game.board[2][2] != 0:
+                return True
+            if self.__game.board[0][2] == self.__game.board[1][1] == self.__game.board[2][0] != 0:
                 return True
 
-    def __declare_winner(self):
+    def __check_draw(self):
+        for row in self.__game.board:
+            if 0 in row:
+                return False
+        return True
+
+    def __declare_winner(self, is_draw: bool):
         self.__game.state = 'finished'
-        self.__game.winner = self.__movement['value']
+        if is_draw:
+            self.__game.winner = 0
+        else:
+            self.__game.winner = self.__movement['value']
 
     def __update_movements(self):
         self.__game.movements = self.__game.movements or []
