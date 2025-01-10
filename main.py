c75a44377ed843cf9b155718268281c30fe29776
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-10 18:39:47 -0300

    feat: migrate tables added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from app.interface.api import create_app
 from app.infra.settings import EnvVarsHandler
+from app.infra.db.settings import DBMigrationsHandler, DBConnectionHandler
+from app.infra.db.entities import Base
 
 EnvVarsHandler.read_env_vars('.env')
+database = DBConnectionHandler(EnvVarsHandler.get_db_connection_string())
+DBMigrationsHandler.migrate_all_tables(engine=database.get_engine(), base=Base)
 app = create_app()
 
 if __name__ == '__main__':
