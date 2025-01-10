80508138e4df50dc2e70f25f71c89fdb643f4bf0
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-10 16:21:24 -0300

    modify: package importations to use cases in application

--- app/application/use_cases/__init__.py
+++ app/application/use_cases/__init__.py
@@ -1,3 +1,5 @@
 from app.application.use_cases.player_creator import PlayerCreator
 from app.application.use_cases.game_starter import GameStarter
 from app.application.use_cases.game_move_maker import GameMoveMaker
+from app.application.use_cases.ml_trainer import MLTrainer
+from app.application.use_cases.ml_move_maker import MLMoveMaker
