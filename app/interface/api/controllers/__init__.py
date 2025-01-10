8c230e3f7a0d3b0e4c7f4a36373275d8fbfefa79
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-10 18:23:14 -0300

    create: api controllers package to interface layer

---
+++ app/interface/api/controllers/__init__.py
@@ -0,0 +1,3 @@
+from app.interface.api.controllers.player_controller import PlayerController
+from app.interface.api.controllers.game_controller import GameController
+from app.interface.api.controllers.ai_controller import AIController
