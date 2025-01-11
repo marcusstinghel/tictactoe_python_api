d64beb912866bb9bd0a13573c7738d59801ec79c
Marcus Stinghel <vini.o.stinghel@gmail.com>
2025-01-11 00:08:00 -0300

    feat: error handling applied

--- app/interface/api/controllers/game_controller.py
+++ app/interface/api/controllers/game_controller.py
@@ -12,31 +12,37 @@ class GameController:
 
     @staticmethod
     def start_game():
-        data = request.json
-        game_repository = GameRepository()
-        game = GameStarter(game_repository=game_repository)
-        new_game = game.start(
-            player_id=data['player_id'],
-        )
-        return jsonify(new_game.__dict__), 201
+        try:
+            data = request.json
+            game_repository = GameRepository()
+            game = GameStarter(game_repository=game_repository)
+            new_game = game.start(
+                player_id=data['player_id'],
+            )
+            return jsonify(new_game.__dict__), 201
+        except Exception as e:
+            return jsonify({'message': str(e)}), 500
 
     @staticmethod
     def make_move():
-        data = request.json
-        game_repository = GameRepository()
-        player_repository = PlayerRepository()
-        game = game_repository.get_in_progress_game(
-            player_id=data['player_id'],
-        )
-        player = player_repository.get_player(
-            id=data['player_id'],
-        )
-        game_service = GameService(game=game)
-        player_service = PlayerService(player=player)
-        game_move_maker = GameMoveMaker(game_repository=game_repository, game_service=game_service,
-                                        player_repository=player_repository, player_service=player_service)
-        response = game_move_maker.make_move(
-            game=game,
-            movement=ast.literal_eval(json.dumps(data['movement']))
-        )
-        return jsonify(response), 200
+        try:
+            data = request.json
+            game_repository = GameRepository()
+            player_repository = PlayerRepository()
+            game = game_repository.get_in_progress_game(
+                player_id=data['player_id'],
+            )
+            player = player_repository.get_player(
+                id=data['player_id'],
+            )
+            game_service = GameService(game=game)
+            player_service = PlayerService(player=player)
+            game_move_maker = GameMoveMaker(game_repository=game_repository, game_service=game_service,
+                                            player_repository=player_repository, player_service=player_service)
+            response = game_move_maker.make_move(
+                game=game,
+                movement=ast.literal_eval(json.dumps(data['movement']))
+            )
+            return jsonify(response), 200
+        except Exception as e:
+            return jsonify({'message': str(e)}), 500
